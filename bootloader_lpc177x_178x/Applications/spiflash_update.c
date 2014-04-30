1354712a84b1c2f91e5cae2af3d529973e3be4f7
nongxiaoming <nongxiaoming@gmail.com>
2014-04-30 11:45:24 +0800

    update the spi_flash update.

--- bootloader_lpc177x_178x/Applications/spiflash_update.c
+++ bootloader_lpc177x_178x/Applications/spiflash_update.c
@@ -7,28 +7,29 @@
 
 /** The origin buffer on RAM */
 #define BUFF_SIZE           4096
-//#ifdef __IAR_SYSTEMS_ICC__
-//#pragma data_alignment=4
-//uint8_t buffer[BUFF_SIZE];
-//#else
-//uint8_t __attribute__ ((aligned (4))) buffer[BUFF_SIZE];
-//#endif
+#define RBIGU4(p)       ((p)[0]<<24|(p)[1]<<16|(p)[2]<<8|(p)[3])
+
 #define UPDATE_INFO_ADDR 897*4096
 #define UPDATE_DATA_ADDR 898*4096
 struct Update_Info
 {
- uint32_t magic;
+ int32_t magic;
  uint32_t size;
 };
 static uint8_t info_buf[256];
-struct Update_Info *info;
+struct Update_Info info;
 int spiflash_check_update(void)
 {
+
 	SPI_FLASH_Init();
-	SPI_FLASH_BufferRead(info_buf,UPDATE_INFO_ADDR,BUFF_SIZE);
-  info=(struct Update_Info *)info_buf;
-	if(info->magic==NOCRP)
+	SPI_FLASH_BufferRead(info_buf,UPDATE_INFO_ADDR,256);
+  info.size= RBIGU4(info_buf);
+	info.magic= RBIGU4(info_buf+4);
+//		 printf("size:%d\r\n",info.size);
+//	printf("magic:%x\r\n",info.magic);
+	if(info.magic==NOCRP)
 {
+	//printf("magic:%x\r\n",info.magic);
  return 1;
 }else{
  return 0;
@@ -36,8 +37,9 @@ int spiflash_check_update(void)
 }
 void spiflash_update(void)
 {
-	uint32_t faddr=USER_END_SECTOR_ADDRESS,data_addr=UPDATE_DATA_ADDR;
-	uint32_t len=info->size;
+	uint16_t index=0;
+	uint32_t faddr=USER_START_SECTOR_ADDRESS,data_addr=UPDATE_DATA_ADDR;
+	int32_t len=info.size;
 	uint8_t *buffer=(uint8_t*)RAM_START;
 	IAP_STATUS_CODE ret;
   __disable_irq();
@@ -45,24 +47,32 @@ void spiflash_update(void)
 	ret=PrepareSector(USER_START_SECTOR,USER_END_SECTOR);
 	if(ret!=CMD_SUCCESS)
 	{
-
+  // printf(" Prepare sectors error!\r\n");
 	}
 /* ²Á³ýAPPÇøÓò */
 		ret=EraseSector(USER_START_SECTOR,USER_END_SECTOR);
 	if(ret!=CMD_SUCCESS)
 	{
-
+  // printf(" Erase sectors error!\r\n");
 	}
+	
 	while(len>0){
 		SPI_FLASH_BufferRead(buffer,data_addr,BUFF_SIZE);
-	ret=CopyRAM2Flash(faddr,(uint32_t)buffer,BUFF_SIZE);
+	ret=CopyRAM2Flash(faddr,(uint32_t)RAM_START,BUFF_SIZE);
 	if(ret!=CMD_SUCCESS)
 	{
-
+ //printf("copy to flash error:%d,index:%d!\r\n",faddr,index);
 	}
+	index++;
 	faddr+=BUFF_SIZE;
 	data_addr+=BUFF_SIZE;
 	len-=BUFF_SIZE;
 }
+if(ret==CMD_SUCCESS)
+	{
+	//	printf("write to flash success\r\n");
+  SPI_FLASH_SectorErase(UPDATE_INFO_ADDR);
+	//	SPI_Flash_PowerDown();
+  }
 	__enable_irq();
 }
